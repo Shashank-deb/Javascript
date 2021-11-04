dce3fb3b287307ddcb104187b801d484a2d33a3a
Shashank Sharma <sharmashashank446@gmail.com>
2021-11-04 12:17:10 +0530

    Updated the Javascript Folder

---
+++ introduction/Calculator.js
@@ -0,0 +1,30 @@
+function calculator(num1,num2)
+{
+    return num1+num2;
+}
+function calculator(num1,num2)
+{
+    return num1-num2;
+}
+function calculator(num1,num2)
+{
+    return num1*num2;
+}
+function add(num1,num2)
+{
+    return num1+num2;
+}
+function sub(num1,num2)
+{
+    return num1-num2;
+}
+function multi(num1,num2)
+{
+    return num1*num2;
+}
+function calculator(num1,num2,operator)
+{ 
+    return operator(num1,num2); 
+}
+
+calculator(5,6,multi);
